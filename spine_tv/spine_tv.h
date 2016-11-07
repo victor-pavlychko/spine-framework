f0566db9dba6804fb610579bf3c8a202e70153c2
Andrii Novoselskyi <andrii.novoselskyi@timecode.co>
2016-11-07 18:11:14 +0200

    added tvOS framework target

---
+++ spine_tv/spine_tv.h
@@ -0,0 +1,19 @@
+//
+//  spine_tv.h
+//  spine_tv
+//
+//  Created by Andrii Novoselskyi on 11/7/16.
+//  Copyright © 2016 Timecode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for spine_tv.
+FOUNDATION_EXPORT double spine_tvVersionNumber;
+
+//! Project version string for spine_tv.
+FOUNDATION_EXPORT const unsigned char spine_tvVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <spine_tv/PublicHeader.h>
+
+
